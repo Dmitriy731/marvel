1668bb473b196adf9a773ed2c1c764401ab542e6
Dmitriy <Evgrashin.Dmitriy@yandex.ru>
2022-12-07 16:49:54 +0400

    add build

---
+++ build/static/js/8.5ce16876.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmarvel=this.webpackJsonpmarvel||[]).push([[8],{58:function(n,o,e){"use strict";function t(){console.log("Hello world")}function c(){console.log("hw2")}e.r(o),e.d(o,"default",(function(){return t})),e.d(o,"secondLog",(function(){return c}))}}]);
+//# sourceMappingURL=8.5ce16876.chunk.js.map
